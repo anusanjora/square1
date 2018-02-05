5789acc6a3ef3e2dfb7859570aa7880171695427
Aniket-Batabyal <aniketbatabyal1996@gmail.com>
2018-02-05 19:49:33 +0530

    SUCCESSFUL COMMIT 1: ADDED NAME AND PASSWORD AND SIGNIN BUTTON.
    FUTURE COMMITS WILL INCLUDE:
    2. SETTING JAVA VERIFICATION FOR LOGIN
    1.addition of Facebook and Google sign in options

--- app/src/main/java/com/square1/ubuntu/square1/SignIn.java
+++ app/src/main/java/com/square1/ubuntu/square1/SignIn.java
@@ -1,8 +1,8 @@
 package com.square1.ubuntu.square1;
 
 import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +13,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.rengwuxian.materialedittext.MaterialEditText;
 import com.square1.ubuntu.square1.Model.User;
 
 public class SignIn extends AppCompatActivity {
@@ -24,8 +23,8 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-    edtPassward = (MaterialEditText)findViewById(R.id.edtPassward) ;
-    edtID = (MaterialEditText)findViewById(R.id.edtID);
+    edtPassward = findViewById(R.id.edtPassward) ;
+    edtID = findViewById(R.id.edtID);
     btnSignIn = (Button)findViewById(R.id.btnSignIn);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
